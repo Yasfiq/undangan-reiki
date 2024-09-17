3bb1e6fb1af6b1457b35cb3bd309ccba32a8a318
Yasfiq <mohammadyasfiq31@gmail.com>
2024-09-18 05:53:05 +0700

    feat: add throttling and rotate image

--- src/app/components/FallingFlowers.js
+++ src/app/components/FallingFlowers.js
@@ -2,15 +2,21 @@ import React, { useEffect, useRef } from "react";
 
 const FallingFlowers = () => {
   const flowersRef = useRef([]);
+  const maxFlowers = 20; // Batas maksimal bunga di layar
+  let lastRenderTime = Date.now(); // Untuk throttling animasi
 
   useEffect(() => {
     const container = document.querySelector("#flower-container");
 
     const createFlower = () => {
+      // Cek apakah jumlah bunga di layar sudah mencapai batas maksimal
+      if (flowersRef.current.length >= maxFlowers) return;
+
       const flower = document.createElement("div");
       flower.classList.add("flower");
       flower.style.left = Math.random() * 100 + "vw";
-      flower.dataset.speed = Math.random() * 3 + 5; // Set kecepatan bunga jatuh
+      flower.dataset.speed = Math.random() * 2 + 10; // Kecepatan bunga jatuh
+      flower.style.top = "-10vh"; // Set posisi awal di atas layar
       container.appendChild(flower);
 
       // Simpan referensi ke elemen bunga
@@ -20,19 +26,26 @@ const FallingFlowers = () => {
     // Buat bunga baru setiap 1500ms
     const flowerInterval = setInterval(createFlower, 1500);
 
-    // Fungsi untuk menjalankan animasi dengan requestAnimationFrame
+    // Fungsi untuk menjalankan animasi dengan requestAnimationFrame dan throttling
     const animateFlowers = () => {
-      flowersRef.current.forEach((flower, index) => {
-        const currentTop = parseFloat(flower.style.top || "-10vh"); // Ambil posisi top sekarang
-        const speed = parseFloat(flower.dataset.speed); // Ambil kecepatan dari data atribut
-        flower.style.top = currentTop + speed + "px"; // Update posisi bunga
+      const now = Date.now();
+      const deltaTime = now - lastRenderTime;
 
-        // Hapus bunga jika sudah keluar layar
-        if (currentTop > window.innerHeight) {
-          container.removeChild(flower);
-          flowersRef.current.splice(index, 1); // Hapus dari array
-        }
-      });
+      // Update posisi bunga hanya setiap 100ms untuk mengurangi beban di perangkat mobile
+      if (deltaTime > 50) {
+        flowersRef.current.forEach((flower, index) => {
+          const currentTop = parseFloat(flower.style.top || "-10vh");
+          const speed = parseFloat(flower.dataset.speed);
+          flower.style.top = currentTop + speed + "px"; // Update posisi bunga
+
+          // Hapus bunga jika sudah keluar layar
+          if (currentTop > window.innerHeight) {
+            container.removeChild(flower);
+            flowersRef.current.splice(index, 1); // Hapus dari array
+          }
+        });
+        lastRenderTime = now;
+      }
 
       // Panggil animasi lagi untuk frame berikutnya
       requestAnimationFrame(animateFlowers);
